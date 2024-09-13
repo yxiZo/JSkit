d852a341f57af0109a61cfaf3fa8c2b2bd9af6bd
zhouquan <cri.sri@outlook.com>
2024-09-13 14:04:23 +0800

    chore: temp add sku algo

---
+++ src/algo/sku/index2.ts
@@ -0,0 +1,428 @@
+const testSku: SingleSku[] = [
+    {
+        "amountOnSale": 9954,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331516,
+        "specId": "4bd24beacedefa1ef935697901249873",
+        "cargoNumber": "927黑色39",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927黑色",
+                "valueTrans": "927 black",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01bo1xlT1JfhBrdH2mh_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "39",
+                "valueTrans": "39",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9866,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331517,
+        "specId": "42128990f10b1fe4b924554c1fdf77db",
+        "cargoNumber": "927黑色40",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927黑色",
+                "valueTrans": "927 black",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01bo1xlT1JfhBrdH2mh_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "40",
+                "valueTrans": "40",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9658,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331518,
+        "specId": "67e41fb6aaf08f760deb86f4cbf873c7",
+        "cargoNumber": "927黑色41",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927黑色",
+                "valueTrans": "927 black",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01bo1xlT1JfhBrdH2mh_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "41",
+                "valueTrans": "41",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9622,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331519,
+        "specId": "17b2a0aeded63093fffc1d3e8609cc53",
+        "cargoNumber": "927黑色42",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927黑色",
+                "valueTrans": "927 black",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01bo1xlT1JfhBrdH2mh_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "42",
+                "valueTrans": "42",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9742,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331520,
+        "specId": "cae3edf65e6c676981424d0bb5a3d7d7",
+        "cargoNumber": "927黑色43",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927黑色",
+                "valueTrans": "927 black",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01bo1xlT1JfhBrdH2mh_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "43",
+                "valueTrans": "43",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9967,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331521,
+        "specId": "7f109c4ecb78d1d43cc0b0350880beda",
+        "cargoNumber": "927黑色44",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927黑色",
+                "valueTrans": "927 black",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01bo1xlT1JfhBrdH2mh_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "44",
+                "valueTrans": "44",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9989,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331522,
+        "specId": "de7c48c47a4e40783110af691a4efe5b",
+        "cargoNumber": "927米色39",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927米色",
+                "valueTrans": "927 beige",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01zqagnv1JfhBoArfsT_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "39",
+                "valueTrans": "39",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9955,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331523,
+        "specId": "9d9f6be1fb41d6f405bf9f7335b4bf99",
+        "cargoNumber": "927米色40",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927米色",
+                "valueTrans": "927 beige",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01zqagnv1JfhBoArfsT_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "40",
+                "valueTrans": "40",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9946,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331524,
+        "specId": "552740517ba799747b33536d0c9abb86",
+        "cargoNumber": "927米色41",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927米色",
+                "valueTrans": "927 beige",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01zqagnv1JfhBoArfsT_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "41",
+                "valueTrans": "41",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9928,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331525,
+        "specId": "8431e5514c25f3b741d4b4ba1c41c45e",
+        "cargoNumber": "927米色42",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927米色",
+                "valueTrans": "927 beige",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01zqagnv1JfhBoArfsT_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "42",
+                "valueTrans": "42",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9950,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331526,
+        "specId": "348d1b6de5d84ce3e84dc5756d491b47",
+        "cargoNumber": "927米色43",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927米色",
+                "valueTrans": "927 beige",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01zqagnv1JfhBoArfsT_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "43",
+                "valueTrans": "43",
+                "skuImageUrl": null
+            }
+        ]
+    },
+    {
+        "amountOnSale": 9961,
+        "price": null,
+        "priceUsd": null,
+        "jxhyPrice": "24.0",
+        "skuId": 5500948331527,
+        "specId": "670fa5bad72b30adaef53306c9d8da9c",
+        "cargoNumber": "927米色44",
+        "skuAttributes": [
+            {
+                "attributeId": 3216,
+                "attributeName": "颜色",
+                "attributeNameTrans": "Color",
+                "value": "927米色",
+                "valueTrans": "927 beige",
+                "skuImageUrl": "https://cbu01.alicdn.com/img/ibank/O1CN01zqagnv1JfhBoArfsT_!!3900301056-0-cib.jpg"
+            },
+            {
+                "attributeId": 450,
+                "attributeName": "尺码",
+                "attributeNameTrans": "Size",
+                "value": "44",
+                "valueTrans": "44",
+                "skuImageUrl": null
+            }
+        ]
+    }
+]
+// https://hurui.me/posts/8b2bd6f8/
+interface SkuAttribute {
+    // 属性ID
+    attributeId: number;
+    // 属性名称
+    attributeName: string;
+    // 属性名称翻译
+    attributeNameTrans: string;
+    // 图片
+    skuImageUrl: string;
+    // 值
+    value: string;
+    // 值翻译
+    valueTrans: string;
+}
+interface SkuAttributeCollection {
+    attributeId: number;
+    attributeName: string;
+    attributeNameTrans: string;
+    attributeItem: SkuAttributeItem[];
+}
+interface SkuAttributeItem {
+    // 图片
+    skuImageUrl: string;
+    // 值
+    value: any[];
+    // 值翻译
+    valueTrans: string;
+}
+
+interface SingleSku {
+    // 在售数量
+    amountOnSale: number;
+    // 货号
+    cargoNumber: string;
+    // 价格
+    jxhyPrice: string;
+    // 价格
+    price: null;
+    priceUsd: null;
+    // 属性
+    skuAttributes: SkuAttribute[];
+    // skuId
+    skuId: number;
+    // 规格ID
+    specId: string;
+}
+
+
+class SKU {
+    public skuCollections: any[] = [];
+    public attributeCollection: SkuAttributeCollection[] = [];
+    constructor(props: {
+        skuCollections: any[]
+    }) {
+        this.skuCollections = props.skuCollections
+        this.attributeCollection = this.getSKUAttributeCollection()
+    }
+
+    /**
+     * @description 获取 SKU 属性集合
+     */
+    private getSKUAttributeCollection() {
+        const attributeCollection: SkuAttributeCollection[] = []
+        this.skuCollections.forEach((sku) => {
+            sku.skuAttributes.forEach((attribute: SkuAttribute) => {
+                // 如果属性集合中不存在该属性，则添加
+                // 如果属性集合中存在该属性，则判断是否存在该属性值，如果不存在则添加
+                const attributeIndex = attributeCollection.findIndex((item) => item.attributeId === attribute.attributeId)
+                if (attributeIndex === -1) {
+                    attributeCollection.push({
+                        attributeId: attribute.attributeId,
+                        attributeName: attribute.attributeName,
+                        attributeNameTrans: attribute.attributeNameTrans,
+                        attributeItem: [
+                            {
+                                skuImageUrl: attribute.skuImageUrl,
+                                value: [attribute.value],
+                                valueTrans: attribute.valueTrans
+                            }
+                        ]
+                    })
+                } else {
+                    // 判断属性值是否存在
+                    const isExistValue = attributeCollection.some((item) => item.attributeItem.some((item) => item.value === attribute.value))
+                    if (!isExistValue) {
+                        attributeCollection[attributeIndex].attributeItem.push( {
+                            skuImageUrl: attribute.skuImageUrl,
+                            value: attribute.value,
+                            valueTrans: attribute.valueTrans
+                        })
+                    }
+                }
+            })
+        })
+        return attributeCollection
+    }
+}
+const sku = new SKU({
+    skuCollections: testSku
+})
+console.log(sku.attributeCollection)
